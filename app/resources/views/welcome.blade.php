cb053b5c23cc29301021d071c9f556fe9761f001
Yuri Mataev <yuri@mataev.com>
2020-04-02 03:11:41 -0400

    Minor view tweak

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -48,14 +48,8 @@
                 font-size: 84px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
+            .tasks {
+                text-align: left;
             }
 
             .m-b-md {
